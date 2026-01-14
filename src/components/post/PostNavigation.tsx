7edb2c7f5eee14c5b3750af0d5f6d77dca380af9
Dawid Krasiński <dawidkrasinski14@gmail.com>
2026-01-14 23:55:05 +0100

    Added PostNavigation hover animation

--- src/components/post/PostNavigation.tsx
+++ src/components/post/PostNavigation.tsx
@@ -7,11 +7,16 @@ export default function PostNavigation() {
 
   return (
     <div className="flex justify-between mt-12 pt-8 border-t border-gray-800">
-      <button onClick={() => router.push("/blog")} className="flex gap-2">
-        <ArrowLeft /> Back to blog
+      <button
+        onClick={() => router.push("/blog")}
+        className="flex gap-2 group/btn"
+      >
+        <ArrowLeft className="group-hover/btn:-translate-x-1 transition-transform" />{" "}
+        Back to blog
       </button>
-      <button className="flex gap-2">
-        Next article <ArrowRight />
+      <button className="flex gap-2 group/btn">
+        Next article{" "}
+        <ArrowRight className="group-hover/btn:translate-x-1 transition-transform" />
       </button>
     </div>
   );
